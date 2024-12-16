b41d326e69c1bfabf88700abb20fe8dcb441afe0
SriAlanyaReddy <srialanyareddy@gmail.com>
2024-12-16 14:42:00 +0530

    navigation ith loaction myapp by paasing value to by navigation using useaLocation

--- myapp/src/Components/Login.js
+++ myapp/src/Components/Login.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { useNavigate,useLocation } from 'react-router-dom';
+import React,{useState} from 'react';
+import { useNavigate,useLocation} from 'react-router-dom';
 
 const Login = () => {
+    const [formData,setFormData]=useState({
+        name:"",
+        password:""
+    })
     const location=useLocation();
     //destructioing
     const {name,password}=location.state||{};
@@ -9,10 +13,28 @@ const Login = () => {
     const handleClick=()=>{
         navigate("/about");
     }
+    const handleLogin=()=>{
+        //i want to check if user details in singup are asme as login details
+        if(formData.name==name && formData.password==password){
+            alert("successfully logged in");
+         navigate("/");
+        }
+        else{
+            alert("Invalid username or password");
+        }
+    }
+    const handleChange=(e)=>{
+        setFormData({...formData,[e.target.id]:e.target.value})
+    }
   return (
     <div>
         <h1>you are in login page hi!</h1>
         <h2>hello hi {name} your password is {password}</h2>
+        <input type="text" id="name" placeholder="your name" value={formData.name}
+        onChange={handleChange} required/>
+        <input type="password" id="password" placeholder="your password" value={formData.password} onChange={handleChange} required/>
+        <button type="submit" onClick={handleLogin}>login button</button>
+        
         <button type="button" onClick={handleClick}>Click it go to about page</button>
     </div>
   )
